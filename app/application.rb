7c7699182fa22d9915f67b2daa79dee6fe506404
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-15 19:05:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
     if Time.new.strftime("%H") > 12
-      resp.write "Good Morning"
+      resp.write "Good Morning!"
+    else
+      resp.erite "Good Afternoon!"
 
 
 end

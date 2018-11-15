b6649020625c276f6415157761f5c8076ffed7f4
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-11-15 19:18:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,19 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    if Time.new.strftime("%H").to_i < 12
+    #if Time.new.strftime("%H").to_i < 12
+    #  resp.write "Good Morning!"
+    #elsif Time.new.strftime("%H").to_i > 12
+    #  resp.write "Good Afternoon!"
+    #end
+
+    t = Time.new
+    if t.hour < 12
       resp.write "Good Morning!"
-    elsif Time.new.strftime("%H").to_i > 12
+    else
       resp.write "Good Afternoon!"
     end
+
     resp.finish
   end
 end
